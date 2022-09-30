6bcbb749774fd8497520a5895e824a6e02057e4d
Pavel <whatisclamp@gmail.com>
2022-09-30 15:26:43 +0300

    feat: add sortby dropdown. add cards base

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 const languageBtn = document.querySelector(".header__dropdown");
-const languageList = document.querySelectorAll(".dropdown-item");
+const languageList = document.querySelectorAll(".header__language .dropdown-item");
+const sortBtn = document.querySelector('.btn-normal');
+const sortBtnArr = document.querySelector('.btn-reverse');
+const sortList = document.querySelectorAll('.catalog__sortby .dropdown-item')
 
 languageList.forEach((item) => {
   item.addEventListener("click", (e) => {
     languageBtn.textContent = e.target.textContent;
   });
 });
+
+sortList.forEach(item => {
+  item.addEventListener('click', (e) => {
+    sortBtn.textContent = e.target.textContent;
+  })
+
+})
 
 let slider = document.getElementById("slider");
 
